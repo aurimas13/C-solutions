38d7e36102e5244513c008c427c4088bafb8903b
Aurimas Nausedas <aurimas.nausedas@gmail.com>
2021-10-31 09:25:49 +0200

    Update Problem_11.c

--- Problem_11.c
+++ Problem_11.c
@@ -1,20 +1,21 @@
 //
 // Problem_11.c
 //
-//  Created by Aurimas Nausedas on 9/20/19.
+// Created by Aurimas Nausedas on 9/20/19.
+// Update by Aurimas Nausedas on 31/10/21.
 
 #include <stdio.h>
 
-// deklaruoju funkcija
+// deklaruoju funkcija | function declaration
 int fibo(int n);
 
 int main()
 {
-    // naudoju for cikla spaudinimui, bet ne Fibonacio skaiciu skaiciavimui
+    // naudoju for cikla spaudinimui, bet ne Fibonacio skaiciu skaiciavimui I use for cycle but not for counting Fibonacci numbers
     for (int n = 0; n < 10; n++)
     {
         int r = fibo(n);
-        //printf("%d-as Fibonacio eiles skaicius yra %d\n",n,r);
+        //printf("%d-as The number of Fibonacci rows is %d\n",n,r);
         printf("%d, ",r);
     }
 
