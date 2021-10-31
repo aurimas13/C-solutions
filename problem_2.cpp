069b03a18e07a73ce5cc392ce86de350a80dd587
Aurimas Nausedas <aurimas.nausedas@gmail.com>
2021-10-31 11:25:50 +0200

    Update problem_2.cpp

--- problem_2.cpp
+++ problem_2.cpp
@@ -1,3 +1,8 @@
+// Problem_2.cpp
+//
+// Created by Aurimas Nausedas on 9/17/19.
+// Updated by Aurimas Nausedas on 10/31/21.
+
 #include <iostream>
 
 using namespace std;
@@ -6,8 +11,8 @@ int main()
 {
   float y;
   int i;
-  cout << "\t" << "*** Table ***" << endl << endl // pavadinimas
-       << "x" << "\t" << "x^2" << "\t" << "x^3" << "\t" << "1/x" << endl; // nupiesti kriterijai
+  cout << "\t" << "*** Table ***" << endl << endl // pavadinimas | name
+       << "x" << "\t" << "x^2" << "\t" << "x^3" << "\t" << "1/x" << endl; // nupiesti kriterijai | drawn requirements
   for(i=1, y=1; i<=10 && y<=10; i++, y++) // for loopas dvejiems int
   {
     printf("%d\t%d\t%d\t%.2f\n", i, i*i, i*i*i, 1/y);
