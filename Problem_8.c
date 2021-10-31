8d86228fa9d83dc55c00d233e95e9f3a401241d8
Aurimas Nausedas <aurimas.nausedas@gmail.com>
2021-10-31 11:03:48 +0200

    Update Problem_8.c

--- Problem_8.c
+++ Problem_8.c
@@ -1,12 +1,13 @@
 // Problem_8.c
 //
-//  Created by Aurimas Nausedas on 9/20/19.
+// Created by Aurimas Nausedas on 9/20/19.
+// Updated by Aurimas Nausedas on 10/31/21.
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
 
-// deklaruoju funkcijas
+// deklaruoju funkcijas | declaring functions
 double rnd(void);
 double fk(double x);
 
@@ -15,20 +16,20 @@ double fk(double x);
 int main()
 {
 
-    // deklaruotu variables
+    // deklaruoju variables | declaring variables
     long i,
          n = 0,
-         max = 5; // pratinis iteraciju skaicius
+         max = 5; // pradinis iteraciju skaicius | initial number of iterations
 
-    // trims skirtingams metodams 3 skirtingos sumos si
+    // trims skirtingams metodams 3 skirtingos sumos si | for three different methods 3 different amounts si
     long s1 = 0;
     double s2 = 0,
            s3 = 0,
            ref = 0.5;
 
-    // atspaudinu pirma eilute
-    printf("*** Integralas ( I - 0.5 ): Monte Karlo metodai\n\n");
-    printf("Tikslus rezultatas = 0.0250\n\n");
+    // atspaudinu pirma eilute | print the first line
+    printf("*** Integralas ( I - 0.5 ): Monte Carlo methods\n\n");
+    printf("Exact result = 0.0250\n\n");
     printf("%8s %8s %8s %8s\n\n","Iter.","I","II","III");
 
     for (int j = 0; j < 13; j++)
@@ -36,23 +37,23 @@ int main()
         for (i = 0; i < max; i++)
         {
 
-             // Monte Karlo algoritmas 1
-            // Klausimas: ka daryti su funkcija
-            // kurios f(x) vertes virsija 1?
-            // siuo atveju taip nera.
-            // spresciau "suspaudamas" funkcija
-            // po to rezultata daugindamas is suspaudino..
+            // Monte Karlo algoritmas 1 | Monte Carlo Algorithm 1
+            // Klausimas: ka daryti su funkcija | Question: what to do with a function
+            // kurios f(x) vertes virsija 1? | which values of f (x) exceed 1?
+            // siuo atveju taip nera. | this is not the case.
+            // spresciau "suspaudamas" funkcija | more "compressing" function
+            // po to rezultata daugindamas is suspaudino.. | then multiply the result by compressing ..
 
-            if(rnd() < fk(rnd())) // palyginame f<f(x)
+            if(rnd() < fk(rnd())) // palyginame/comapre f<f(x) 
              {
              s1++;
              }
 
-             // Monte Karlo algoritmas 2
+             // Monte Karlo algoritmas 2 | Monte Carlo Algorithm 2
              s2 += fk(rnd()); // s += f(x)
 
 
-            // Monte Karo algoritmas 3
+            // Monte Karo algoritmas 3 | Monte Carlo Algorithm 3
             double x = sqrt (rnd());
             s3+=(fk(x)/2/x); //s+=F(x)
 
