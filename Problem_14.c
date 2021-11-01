92b378be5c735ec6d65d60617a9318e85d2bc53e
Aurimas A. Nausėdas <aurimas.nausedas@gmail.com>
2021-11-01 07:32:46 +0200

    Update Problem_14.c

--- Problem_14.c
+++ Problem_14.c
@@ -1,8 +1,8 @@
 //
 // Problem_14.c
 //
-// Created by Aurimas Nausedas on 9/23/19.
-// Updated by Aurimas Nausedas on 31/10/21.
+// Created by Aurimas A. Nausedas on 9/23/19.
+// Updated by Aurimas A. Nausedas on 10/31/21.
 
 
 #include <stdio.h>
