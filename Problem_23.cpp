d52d39c61e3eef89822590c225f06e05de99365e
Aurimas A. Nausėdas <aurimas.nausedas@gmail.com>
2021-11-01 07:38:37 +0200

    Update Problem_23.cpp

--- Problem_23.cpp
+++ Problem_23.cpp
@@ -1,8 +1,8 @@
 //
 // Problem_23.cpp
 //
-// Created by Aurimas Nausedas on 10/20/19.
-// Updated by Aurimas Nausedas on 10/31/21.
+// Created by Aurimas A. Nausedas on 10/20/19.
+// Updated by Aurimas A. Nausedas on 10/31/21.
 
 #include <iostream>
 #include <iomanip>
