af92c5220b92bc003dfaaf643ce30f25b2a5024f
Aurimas A. Nausėdas <aurimas.nausedas@gmail.com>
2021-11-01 07:35:03 +0200

    Update Problem_18.cpp

--- Problem_18.cpp
+++ Problem_18.cpp
@@ -1,8 +1,8 @@
 //
 // Problem_18.cpp
 //
-// Created by Aurimas Nausedas on 9/20/19.
-// Updated by Aurimas Nausedas 10/31/21.
+// Created by Aurimas A. Nausedas on 9/20/19.
+// Updated by Aurimas A. Nausedas 10/31/21.
 
 #include <stdio.h>
 #include <conio.h>
