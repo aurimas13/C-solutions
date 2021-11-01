a03e5b09c6ec2ca8cbe6658129a3e49f2b670adf
Aurimas A. Nausėdas <aurimas.nausedas@gmail.com>
2021-11-01 07:41:31 +0200

    Update Problem_8.c

--- Problem_8.c
+++ Problem_8.c
@@ -1,7 +1,7 @@
 // Problem_8.c
 //
-// Created by Aurimas Nausedas on 9/20/19.
-// Updated by Aurimas Nausedas on 10/31/21.
+// Created by Aurimas A. Nausedas on 9/20/19.
+// Updated by Aurimas A. Nausedas on 10/31/21.
 
 #include <stdio.h>
 #include <stdlib.h>
